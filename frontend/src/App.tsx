a55fbde9828c549bf8c065ff665909bf62c83da8
MateuszKitzol <mateusz@kitzol.pl>
2025-07-11 18:50:50 +0200

    Improve chat input design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -51,7 +51,7 @@ function App() {
             onChange={(e) => setMessage(e.target.value)}
             onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
           />
-          <button onClick={sendMessage}>Send</button>
+          <button onClick={sendMessage}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-send"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg></button>
         </div>
       </header>
     </div>
